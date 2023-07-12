7641e2a86c6a1773790461c6969c5e61d1a65fbf
Bellaala Mohamed <mohamed.bellaala@nestle.com>
2023-07-12 14:17:11 +0100

    Initial Commit

---
+++ Bmo/BmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BmoApp.swift
+//  Bmo
+//
+//  Created by Bellaala Mohamed on 12/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct BmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
